a1cea6c7d6f50a377f865b4d86a3e524ecc6a7c5
Jim Nelson <jim@jimnelson.us>
2022-03-23 21:48:33 -0700

    starting to port main.js

--- vanillajs/assets/js/main.js
+++ vanillajs/assets/js/main.js
@@ -1,1 +1,26 @@
 // Let's try vanillajs instead
+
+/*
+    Original main.js notes:
+
+    set references to:
+    - window
+    - body
+*/
+
+const body = document.querySelector('body');
+
+/*
+    set breakpoints using 3rd-party library
+        appears to like media queries
+        triggers events at screen sizes set by this library
+        unsure if this project is using any of those events
+*/
+
+// Skip...not sure if we need this
+
+// Original theme removes is-preload class from body 100ms after load
+// Appears to change styling of tiles and hold animations
+document.addEventListener('DOMContentLoaded', () =>
+  body.classList.remove('is-preload')
+);
